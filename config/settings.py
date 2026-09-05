9d9f0a623c7f27662455db24babaed564bee3b0d
MERCY3cloud <akirimercy3@gmail.com>
2026-09-05 13:20:39 +0300

    Update Reflex deployment settings

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("T2hslzEkCDyt_8hauiv5YzGj2YlOJIARK2DkUXDgfuYy1fWaZrKPmxOud4KyihSwzzc")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
