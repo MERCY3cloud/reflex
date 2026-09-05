7bd81bbe086dc8908e3678d1248f081a476f2486
MERCY3cloud <akirimercy3@gmail.com>
2026-09-05 14:28:16 +0300

    Update production API URL

--- config/settings.py
+++ config/settings.py
@@ -20,19 +20,19 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     "corsheaders",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'rest_framework',
+    "rest_framework",
 
-    'users',
-    'deliveries',
-    'assignments',
-    'tracking',
+    "users",
+    "deliveries",
+    "assignments",
+    "tracking",
 ]
 
 REST_FRAMEWORK = {
@@ -43,13 +43,13 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # CORS settings - allow frontend dev server origins
@@ -133,3 +133,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+CORS_ALLOWED_ORIGINS = [
+    "https://reflex-bcm00mntm-mercy-akiri-s-projects.vercel.app",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://reflex-bcm00mntm-mercy-akiri-s-projects.vercel.app",
+]
